8921d4e3848c3cd6ec4154c5221eb5c1a0e44244
مصطفى شملي <mstfa@mindPalace>
2019-01-29 01:54:08 -0500

    damage is now saved

--- scripts/healthList.gd
+++ scripts/healthList.gd
@@ -2,6 +2,8 @@ extends MenuButton
 
 var Damage = [ ' ', '/', '✶X' , '✶✶✶✶✶*' ]
 
+onready var sheet = get_node('/root/base')
+
 func populate():
 	if !get_popup().items :
 		for i in Damage:
@@ -9,9 +11,12 @@ func populate():
 
 func set_text(i):
 	text = Damage[i]
+	save()
+	sheet.call('save_data')
 
 func save():
-	pass
+	var data = text
+	return data
 #	for i in Damage:
 #		get_text()
 
@@ -20,3 +25,4 @@ func _ready():
 	get_popup().connect('id_pressed', self, 'set_text')
 	PhysicsServer.set_active(false)
 	Physics2DServer.set_active(false)
+	add_to_group('saveData')
